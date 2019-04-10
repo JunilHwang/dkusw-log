ba78865810594e656d97a7c82c72193c0a6b041b
JunilHwang <tjsdlf4261@naver.com>
2019-04-10 21:43:29 +0900

    #2 package install

--- server.nest/src/app.module.ts
+++ server.nest/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import controllers from './controller';
+import providers from './service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers,
+  providers,
 })
 export class AppModule {}
--- server.nest/src/controller/app.controller.ts
+++ server.nest/src/controller/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../service/app.service';
+import { getDefaultSettings } from 'http2';
 
 @Controller()
 export class AppController {
